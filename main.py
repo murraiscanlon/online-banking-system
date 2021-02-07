c9c7bafa238a8217c743f48ce44a45333b97622b
Murrai Scanlon <50279093+murraiscanlon@users.noreply.github.com>
2021-02-07 12:58:30 -0600

    import_and_create_dictionary(filename) is working

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import re
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def import_and_create_dictionary(filename):
+    '''
+    This function is used to create a bank dictionary.  The given argument is the filename to load.
+    Every line in the file will look like
+    key: value
+    Key is a user's name and value is an amount to update the user's bank account with.  The value should be a
+    number, however, it is possible that there is no value or that the value is an invalid number.
 
+    What you will do:
+    - Try to make a dictionary from the contents of the file.
+    - If the key doesn't exist, create a new key:value pair.
+    - If the key does exist, increment its value with the amount.
+    - You should also handle cases when the value is invalid.  If so, ignore that line and don't update the dictionary.
+    - Finally, return the dictionary.
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+    Note: All of the users in the bank file are in the user account file.
+    '''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    d = {}
+
+    # your code here
+    for line in open(filename, 'r+'):
+        pairlist = line.split('\n')
+        pairwords = pairlist[0].split(':')
+        key = pairwords[0].strip()
+        if key != '':
+            # print(f"key: {key}, value: {pairwords[1].strip()}")
+            value = pairwords[1].strip()
+            if bool(re.search(r'\d', value)):
+                # print(f"key2: {key}, value: {value}")
+                value = float(value)
+                if d.keys().__contains__(key):
+                    value += d[key]
+                    d.update({key: value})
+                else:
+                    d[key] = value
+
+    return d
+
+result = import_and_create_dictionary('bank.txt')
+print(f"import_and_create_dictionary, result: {result}")
